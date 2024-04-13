a84845c87eff811dc79597f9827f2c5bafe7a5d1
Kaveesha <kaveeshawijegurusinghe@gmail.com>
2024-04-13 20:25:30 +0530

    ReactApp

---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data={
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            category: 'shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInstock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Nike Slim Pant',
+            slug: 'nike-slim-pant',
+            category: 'pants',
+            image: '/images/p2.jpg',
+            price: 65,
+            countInstock: 8,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality prduct',
+        },
+        {
+            name: 'Adidas Fit Pant',
+            slug: 'adidas-fit-pant',
+            category: 'pants',
+            image: '/images/p3.jpg',
+            price: 60,
+            countInstock: 5,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality prduct',
+        },
+        
+    ],
+};
+export default data;
